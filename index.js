90a8d26d4d3c8ee334bfd25bfc6ae4e9484490f2
TiloueeeTiloueee <lp.leblanc@hotmail.com>
2026-05-31 21:04:22 -0400

    Finished Cube Visualization

--- index.js
+++ index.js
@@ -1,26 +1,85 @@
 // VISUAL CONFIG
 const BG = "#000000"
 const POINTS = "#00ff00"
+const LINES = "#00ff00"
 const pSize = 15 
 Visualizer.width = 800
 Visualizer.height = 800
+const FPS = 60
+
+//////////////////////////////////////////
 
 // Get Context
 const CTX = Visualizer.getContext("2d")
 
+//////////////////////////////////////////
+// WORK VARS/CONSTS
 
+// Frame Values
+const dt = 1/FPS
+let dz = 1
+
+// Visualizing Values
+const points = [
+    // Plane 1
+    {x: 0.25, y: 0.25, z: 0.25},
+    {x: -0.25, y: 0.25, z: 0.25},
+    {x: -0.25, y: -0.25, z: 0.25},
+    {x: 0.25, y: -0.25, z: 0.25},
+
+    // Plane 2
+    {x: 0.25, y: 0.25, z: -0.25},
+    {x: -0.25, y: 0.25, z: -0.25},
+    {x: -0.25, y: -0.25, z: -0.25},
+    {x: 0.25, y: -0.25, z: -0.25},
+]
+
+const faces = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7,],
+    [0, 4],
+    [2, 6],
+    [1, 5],
+    [3, 7],
+]
+
+// Animation Values
+let angle = 0
+
+//////////////////////////////////////////
 // FUNCTIONS
 
-// UTIL
+// Util
 function clear() {
     CTX.fillStyle = BG
     CTX.fillRect(0, 0, Visualizer.width, Visualizer.height)
 }
 
-// WORK
+function translate_z({x, y, z}) {
+    return {x: x, y: y, z: z + dz}
+}
+
+function rotate_on_y({x, y, z}) {
+    const c = Math.cos(angle)
+    const s = Math.sin(angle)
+    return {
+        x: x * c - z * s,
+        y,
+        z: x * s + z * c,
+    }
+}
+// Work
 function point({x, y}) {
     CTX.fillStyle = POINTS
     CTX.fillRect(x - pSize / 2, y - pSize / 2, pSize, pSize)
+}
+
+function line(p1, p2) {
+    CTX.strokeStyle = LINES
+    CTX.beginPath()
+    CTX.moveTo(p1.x, p1.y)
+    CTX.lineTo(p2.x, p2.y)
+    CTX.stroke()
 }
 
 function screen(p) {
@@ -30,6 +89,34 @@ function screen(p) {
     }
 }
 
+function project({x, y, z}) {
+    return {
+        x: x / z,
+        y: y / z,
+    }
+}
+
+function render() {
+    //dz += 1*dt
+    angle += 2*Math.PI*dt / 8
+    clear()
+    // Rendering Points
+    for (const p of points) {
+        point(screen(project(translate_z(rotate_on_y(p)))))
+    }
+    // Rendering Lines
+    for (const f of faces) {
+        for (let i=0; i < f.length; ++i) {
+            const a = points[f[i]]
+            const b = points[f[(i+1)%f.length]]
+            line(screen(project(translate_z(rotate_on_y(a)))),
+            screen(project(translate_z(rotate_on_y(b)))))
+            
+        }
+    }
+    
+    setTimeout(render, 1000/FPS);
+}
+//////////////////////////////////////////
 // START
-clear()
-point(screen({x: 0, y: 0.5}))
+setTimeout(render, 1000/FPS);
